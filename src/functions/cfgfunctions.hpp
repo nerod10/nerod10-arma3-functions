f13e56ed9b113a9e771715a6e3aa908aa4d088c5
nerod10 <ashinoy@gmail.com>
2023-05-10 16:53:41 +0300

    adding src files

---
+++ src/functions/cfgfunctions.hpp
@@ -0,0 +1,14 @@
+class TG 
+{
+	tag = "TG";
+	class clients
+	{
+		file = "functions\clients";
+		class laserBarrage {};
+		class moveObject {};
+		class fireLaser {};
+		class mouseShipMovment {};
+	}
+	
+
+}
